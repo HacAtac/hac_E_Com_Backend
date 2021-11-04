c4eefbb8110dcacb183a1e0b9a33322868e4ec11
HacAtac <jhack00@icloud.com>
2021-11-04 15:56:42 -0500

    created .env file with secure info defined category column

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.JAWSDB_URL // this is the connection string to the database on heroku (which is the database on my local machine)
+  ? new Sequelize(process.env.JAWSDB_URL) //  if the connection string is on heroku, use that one
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      // if the connection string is on my local machine, use that one
+      host: "root", //process.env.DB_HOST, //"localhost",
+      dialect: "mysql", // this is the default port for mysql   //process.env.DB_DIALECT,
       dialectOptions: {
         decimalNumbers: true,
       },
--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // number (int) type of data in the column (id) in the table (categories) in the database
+      allowNull: false, // not null in the database (categories) in the table (categories) in the database (categories)
+      primaryKey: true, //  primary key in the database (categories) in the table (categories) in the database (categories)
+      autoIncrement: true, // auto increment in the database (categories) in the table (categories) in the database (categories)
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
