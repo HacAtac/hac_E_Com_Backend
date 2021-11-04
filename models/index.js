5e91d9cf8ecd17999ff4c4472ac032b979f60407
HacAtac <jhack00@icloud.com>
2021-11-04 16:37:25 -0500

    created associations for model tables

--- models/index.js
+++ models/index.js
@@ -5,14 +5,15 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category); // this is the equivalent of a join table in SQL (one-to-many)
 // Categories have many Products
-
+Category.hasMany(Product); // this is the equivalent of a join table in SQL (many-to-many)
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: ProductTag }); // this is the equivalent of a join table in SQL (many-to-many)
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag }); // this is the equivalent of a join table in SQL (many-to-many)  // this is the equivalent of a join table in SQL (many-to-many)
 module.exports = {
+  // export all models to be used in the app (index.js)
   Product,
   Category,
   Tag,
